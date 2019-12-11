759b570fbcd290600ff2f2f936fb25cd7a5fdf93
Vladyslav Ziberov <siegbertov@gmail.com>
2019-12-11 11:14:50 +0200

    Created Main Window

---
+++ contact_list.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import messagebox
+import sqlite3
+
+root = Tk()
+root.title("Numbers Notebooks")
+root.iconbitmap("Photo\\Contacts.ico")
+root.configure(background="white")
+dtbs = "contact_list.db"
+
+root.mainloop()
