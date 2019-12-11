c40344ef04aacb1ee0301072dc6518a24ea3bfd8
Vladyslav Ziberov <siegbertov@gmail.com>
2019-12-11 11:25:29 +0200

    Created Commands for Buttons

--- contact_list.py
+++ contact_list.py
@@ -2,17 +2,142 @@ from tkinter import *
 from tkinter import messagebox
 import sqlite3
 
+
 def submit():
-    pass
+    name = f_name.get()
+    surname = l_name.get()
+    phone_number = number.get()
+    if name == "" or surname == "" or phone_number == "":
+        messagebox.showerror("Info", "You forget something")
+    elif not phone_number.isdigit():
+        messagebox.showerror("Info", "Number Should Contain Only Digits")
+    else:
+        conn = sqlite3.connect(dtbs)
+        c = conn.cursor()
+
+        c.execute("""CREATE TABLE IF NOT EXISTS people (
+                        f_name text,
+                        l_name text,
+                        number text
+                        )""")
+
+        c.execute("INSERT INTO people VALUES (:f_name, :l_name, :number)",
+                  {
+                      "f_name": f_name.get(),
+                      "l_name": l_name.get(),
+                      "number": number.get()
+                  })
+
+        conn.commit()
+        conn.close()
+        txt = "You added:\n {} {} - {}".format(f_name.get(), l_name.get(), number.get())
+        messagebox.showinfo("Info", txt)
+
+        f_name.delete(0, END)
+        l_name.delete(0, END)
+        number.delete(0, END)
+
 
 def delete():
-    pass
+    conn = sqlite3.connect(dtbs)
+    c = conn.cursor()
+
+    c.execute("DELETE from people WHERE oid = " + del_box.get())
+
+    del_box.delete(0, END)
+
+    conn.commit()
+    conn.close()
+
 
 def edit():
-    pass
+    global editor
+    editor = Tk()
+    editor.title("Numbers Notebooks")
+    editor.configure(background="white")
+
+    conn = sqlite3.connect(dtbs)
+    c = conn.cursor()
+
+    c.execute("SELECT * FROM people WHERE oid  = " + del_box.get())
+    records = c.fetchall()
+
+    f_name_lbl_editor = Label(editor, text="Name", bg="grey80").grid(row=0, column=0, padx=20, pady=5)
+    l_name_lbl_editor = Label(editor, text="Surname", bg="grey80").grid(row=1, column=0, padx=20, pady=5)
+    number_lbl_editor = Label(editor, text="Number", bg="grey80").grid(row=2, column=0, padx=20, pady=5)
+
+    global f_name_editor, l_name_editor, number_editor
+
+    f_name_editor = Entry(editor, width=30)
+    f_name_editor.grid(row=0, column=1, padx=20)
+    l_name_editor = Entry(editor, width=30)
+    l_name_editor.grid(row=1, column=1, padx=20)
+    number_editor = Entry(editor, width=30)
+    number_editor.grid(row=2, column=1, padx=20)
+
+    for record in records:
+        f_name_editor.insert(0, record[0])
+        l_name_editor.insert(0, record[1])
+        number_editor.insert(0, record[2])
+
+    conn.commit()
+    conn.close()
+
+    save_btn_editor = Button(editor, bg="grey60", text="Save",
+                             command=save).grid(row=3, column=0, columnspan=2, ipadx=50, pady=(5, 30))
+
+    editor.mainloop()
+
 
 def query():
-    pass
+    conn = sqlite3.connect(dtbs)
+    c = conn.cursor()
+
+    c.execute("SELECT *, oid FROM people")
+    records = c.fetchall()
+    if not records:
+        messagebox.showinfo("Info", "You don't have any contact\n\nPlease add somebody")
+    else:
+        k = 1
+        for item in range(len(records)):
+            st = records[item]
+            name = st[0]
+            surname = st[1]
+            num = st[2]
+            el = st[3]
+            Label(root, text=str(k) + ". " + name + " " + surname + " " + num + " #" + str(el),
+                  bg="white").grid(row=k + 7, column=0, columnspan=2)
+            k += 1
+
+    conn.commit()
+    conn.close()
+
+
+def save():
+    conn = sqlite3.connect(dtbs)
+    c = conn.cursor()
+
+    c.execute("""UPDATE people  SET
+        f_name = :first,
+        l_name = :last,
+        number = :number
+
+        WHERE oid = :oid""",
+
+              {
+                  'first': f_name_editor.get(),
+                  'last': l_name_editor.get(),
+                  'number': number_editor.get(),
+                  'oid': del_box.get()
+              }
+
+              )
+
+    conn.commit()
+    conn.close()
+    editor.destroy()
+    del_box.delete(0, END)
+
 
 root = Tk()
 root.title("Numbers Notebooks")
